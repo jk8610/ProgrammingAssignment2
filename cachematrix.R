4b7d11c8eacc9759ac646a08453665e97f2b67a9
jk8610 <jkk8610@gmail.com>
2016-09-04 22:28:50 +1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## this function creates a compound object which is based on its input,
+## a user-nominated matrix. the returned result is a list of four(4) 
+## functions: set(), get(), setreverse() and getreverse(). the first two
+## functions creates/returns the internal copy of the matrix received as input,
+## the last two creates/returns the reverse of the same matrix, respectively
+makeCacheMatrix<-function(x=matrix()) {
+    my_reverse <- NULL
+	set <- function(y) {
+		x <<- y
+		my_reverse <<- NULL
+	}
+	get <- function() x
+	setreverse <- function(reverse) my_reverse <<- reverse
+	getreverse <- function() my_reverse
+	list(set=set, get=get, setreverse=setreverse, getreverse=getreverse)
+}	
+	
+## this function accepts a 'makeCacheMatrix' object, and returns
+## its inverse, by first examining the object's cache, return it ifotherwise
+## its inverse has already present, otherwise compute it on the fly
+cacheSolve <- function(x,...) {
+	rev <- x$getreverse()
+	if (!is.null(rev)) {
+		message("getting cached reverse matrix")
+		return(rev)
+	}
+	rev <- solve(x$get())
+	x$setreverse(rev)
+	return(rev)
 }
